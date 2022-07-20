afd4bb07ae945ca75068bb3f64a69db73a81e008
acharyarupak391 <acharyarupak391@gmail.com>
2022-07-20 15:09:04 +0545

    express app added

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,15 @@
-FROM node:alpine
-COPY . ./app
-CMD node /app/app.js
+FROM node:16
+
+WORKDIR /express_app
+
+COPY package*.json ./
+
+RUN yarn install
+
+COPY . .
+
+ENV PORT=3000
+
+EXPOSE 3000
+
+CMD [ "yarn", "start" ]
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello from app.js");
+const app = require("express")();
+
+app.get("/", (req, res) => res.json({ message: "Docker is easy 🐳" }));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+  console.log(`app listening on http://localhost:${port}`)
+);
+
+// run in docker with port forwarding to 8080
+// docker run -p local_port:container_port <IMAGE_ID>
